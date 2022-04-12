8cf782c7fc9e112756da23137120d5d8b5ef9729
daianaponchon <daiana.ponchon85@gmail.com>
2022-04-12 20:14:06 -0300

    dos archivos nuevos

---
+++ script.js
@@ -0,0 +1,58 @@
+//esto es un alert
+alert("hola mundo")
+/* todo es comentario */
+var alumno = "Daiana"
+let nota = 9
+
+const entidad = "Gobierno CIudad"
+
+///entidad = "nacion"
+
+console.log("el nombre del alumno es"  + alumno +  "y su nota es" + nota)
+
+let nombreApellido = "Juan Batman"
+
+let User = "juan carlos"
+let usuario
+let $user
+let nombre_apellido
+let nombreApellido32
+
+//no vannnn
+/*let 1ernombre
+let nombre-apellido 
+let var super break this
+*/
+
+///string
+
+let str1 = "string con comillas dobles"
+let str2 = 'con comillas simples'
+let str3 = "con comillas francesas"
+
+
+console.log(str1 + "y" + str2)
+
+let str4 =str1 + "y" + str2
+
+console.log(str4)
+
+let str5 = '$(str1) y $(str2)'
+
+/// number
+let num1 = parseFloat (prompt ("mete el primer numero")) 
+let num2 = parseFloat (prompt ("mete el segundo numero"))
+
+// operaciones basicas
+let suma = num1 + num2
+let resta = num1 - num2
+let mult = num1 * num2
+let division = num1/num2
+alert("la suma es:" +suma)
+console.log("la resta es:" +resta)
+console.log("la multiplicacion:" +mult)
+
+document.write("<h1> Javascript </h1>" + mult)
+
+
+
